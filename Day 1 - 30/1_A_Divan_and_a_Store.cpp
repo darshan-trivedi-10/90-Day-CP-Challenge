4b2b1b255376d4afab0f822968b706a05fd89531
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2021-11-28 15:27:01 +0530

    Create 1_A_Divan_and_a_Store.cpp

---
+++ Day 1 - 30/1_A_Divan_and_a_Store.cpp
@@ -0,0 +1,84 @@
+// https://codeforces.com/contest/1614/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+template <typename T>
+void input(T a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    int n;
+    ll r, k, l;
+    cin >> n >> l >> r >> k;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(all(arr));
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= r and arr[i] >= l)
+        {
+            if (arr[i] <= k)
+            {
+                k -= arr[i];
+                ans++;
+            }
+        }
+    }
+    pt(ans)
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // srand(time(NULL));
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // if (ray)
+        // cout << "Case #" << s << ":\n";
+        ans();
+        s++;
+    }
+    return 0;
+}
