5f5f31ca3e2bdb195db95e77af57daff74128a9a
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2021-12-03 19:19:11 +0530

    Create 4_B_AquaMoon_and_Stolen_String.cpp

---
+++ Day 1 - 30/4_B_AquaMoon_and_Stolen_String.cpp
@@ -0,0 +1,79 @@
+// https://codeforces.com/contest/1546/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+template <typename T>
+void input(T &a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<string> arr(n + n - 1);
+    input(arr, n + n - 1);
+    for (int i = 0; i < m; i++)
+    {
+        int cc = 0;
+        for (auto &it : arr)
+        {
+            cc ^= (int)it.at(i);
+        }
+        if (cc != 0)
+        {
+            cout << (char)cc;
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // srand(time(NULL));
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        if (ray)
+            cout << "Case #" << s << ": ";
+        ans();
+        s++;
+    }
+    return 0;
+}
