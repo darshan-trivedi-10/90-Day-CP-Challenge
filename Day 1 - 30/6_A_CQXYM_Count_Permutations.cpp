43c81d8c51bc67557b9528f020b2959faef6187d
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2021-12-06 18:38:54 +0530

    Create 6_A_CQXYM_Count_Permutations.cpp

---
+++ Day 1 - 30/6_A_CQXYM_Count_Permutations.cpp
@@ -0,0 +1,70 @@
+// https://codeforces.com/problemset/problem/1581/A
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+template <typename T>
+void input(T a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    int n;
+    cin >> n;
+
+    ll ans = 1;
+    for (ll i = 3; i <= (2 * n); i++)
+    {
+        ans = (ans * i) % mod;
+    }
+    pt(ans)
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // srand(time(NULL));
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        if (ray)
+            cout << "Case #" << s << ": ";
+        ans();
+        s++;
+    }
+    return 0;
+}
