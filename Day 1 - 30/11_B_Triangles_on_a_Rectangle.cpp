dc2077df574b572e632250a26f435783d0c23cd5
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2021-12-19 10:57:27 +0530

    Add files via upload

---
+++ Day 1 - 30/11_B_Triangles_on_a_Rectangle.cpp
@@ -0,0 +1,165 @@
+/*
+ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
+उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
+
+*/
+
+// https://codeforces.com/contest/1620/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+/*Keep learning
+Keep doing
+keep upsolving*/
+template <typename T>
+void input(T a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    int w, h;
+    cin >> w >> h;
+    ll k;
+
+    ll a2 = 1, a1 = 1, a3 = 1, a4 = 1;
+    cin >> k;
+    for (int i = 0; i < k; i++)
+    {
+        ll x;
+        cin >> x;
+        if (i == 0 || i == (k - 1))
+        {
+            if (i == (k - 1))
+            {
+                a1 = abs(a1 - x);
+                a3 *= h;
+            }
+            else
+            {
+                a1 = x;
+            }
+        }
+    }
+
+    cin >> k;
+    ll cc = 0;
+    for (int i = 0; i < k; i++)
+    {
+        ll x;
+        cin >> x;
+        if (i == 0 || i == (k - 1))
+        {
+            if (i == (k - 1))
+            {
+                cc = abs(cc - x);
+                a3 *= cc;
+                cc = 0;
+            }
+            else
+            {
+                cc = x;
+            }
+        }
+        if (i == (k / 2))
+        {
+
+            a1 *= h;
+        }
+    }
+
+    cin >> k;
+    for (int i = 0; i < k; i++)
+    {
+        ll x;
+        cin >> x;
+        if (i == 0 || i == (k - 1))
+        {
+            if (i == (k - 1))
+            {
+                a4 *= w;
+                a2 = abs(a2 - x);
+            }
+            else
+            {
+                a2 = x;
+            }
+        }
+    }
+
+    cin >> k;
+    for (int i = 0; i < k; i++)
+    {
+
+        ll x;
+        cin >> x;
+        if (i == 0 || i == (k - 1))
+        {
+            if (i == (k - 1))
+            {
+                cc = abs(cc - x);
+                a4 *= cc;
+                cc = 0;
+            }
+            else
+            {
+                cc = x;
+            }
+        }
+        if (i == (k / 2))
+        {
+            a2 *= w;
+        }
+    }
+    pt(max({a1, a2, a3, a4}))
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // srand(time(NULL));
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // if (ray)
+        // cout << "Case #" << s << ":\n";
+        ans();
+        s++;
+    }
+    return 0;
+}
