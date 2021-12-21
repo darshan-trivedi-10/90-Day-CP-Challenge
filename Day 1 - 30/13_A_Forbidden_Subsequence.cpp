46f4d947d4c5937b25c6b84278c2f71064168099
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2021-12-21 14:13:37 +0530

    Add files via upload

---
+++ Day 1 - 30/13_A_Forbidden_Subsequence.cpp
@@ -0,0 +1,89 @@
+// https://codeforces.com/contest/1617/problem/A
+/*
+ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
+उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+/*Keep learning
+Keep doing
+keep upsolving*/
+template <typename T>
+void input(T a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    string s, t;
+    cin >> s >> t;
+
+    sort(all(s));
+
+    if (t != "abc")
+    {
+        pt(s) return;
+    }
+
+    if (count(all(s), 'a') * count(all(s), 'b') * count(all(s), 'c') == 0)
+    {
+        pt(s) return;
+    }
+
+    auto l = lower_bound(all(s), 'b');
+    auto r = upper_bound(all(s), 'c');
+
+    reverse(l, r);
+    pt(s)
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // srand(time(NULL));
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        if (ray)
+            cout << "Case #" << s << ": ";
+        ans();
+        s++;
+    }
+    return 0;
+}
