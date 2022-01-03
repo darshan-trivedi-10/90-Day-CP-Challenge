112f939a2ac5ec2dfa23c0416a23f63c39ed8e7f
Darshan Trivedi <79049134+darshan-trivedi-10@users.noreply.github.com>
2022-01-03 20:03:09 +0530

    Add files via upload

---
+++ Day 1 - 30/25_B_Pashmak_and_Flowers.cpp
@@ -0,0 +1,83 @@
+// https://codeforces.com/problemset/problem/459/B
+/*
+ॐ त्र्यम्बकं यजामहे सुगन्धिं पुष्टिवर्धनम् |
+उर्वारुकमिव बन्धनान्मृत्योर्मुक्षीय माऽमृतात् ||
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+using lli = long long int;
+using ll = long long;
+#define mod 1000000007
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+#define ha cout << "YES \n";
+#define na cout << "NO\n";
+#define all(z) z.begin(), z.end()
+#define pt(s) cout << s << endl;
+#define ff first
+#define ss second
+#define um unordered_map
+#define us unordered_set
+#define nline cout << "\n";
+#define pb emplace_back
+
+/*Keep learning
+Keep doing
+keep upsolving*/
+template <typename T>
+void input(T a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+template <typename O>
+void output(O a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+void ans()
+{
+    int n;
+    cin >> n;
+    ll arr[n];
+    input(arr, n);
+    int mine = *min_element(arr, arr + n), maxe = *max_element(arr, arr + n);
+    int c1 = count(arr, arr + n, mine), c2 = count(arr, arr + n, maxe);
+    cout << maxe - mine << " ";
+    if (mine == maxe)
+    {
+        pt((n * (n - 1)) / 2)
+    }
+    else
+    {
+        pt(c1 * c2)
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int s = 1;
+    bool ray = 0;
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        if (ray)
+            cout << "Case #" << s << ": ";
+        ans();
+        s++;
+    }
+    return 0;
+}
